447efb74b4db21685a46ac3e7d653ae783d24501
Yacine Ferhat <Yacine.frhat@gmail.com>
2019-07-28 23:14:28 +0100

    Needs some style but it's okay

--- src/JokeList.js
+++ src/JokeList.js
@@ -89,7 +89,7 @@ class JokeList extends Component{
                         <span>Dad </span>
                     Jokes</h1>
                     <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"/>
-                    <button onClick={this.handleClick} className="JokeListGetMore"> New Jokes</button>
+                    <button onClick={this.handleClick} className="JokeListGetMore"> Fetch Jokes</button>
                 </div>
                 <div className="JokeListJokes">
                     {jokes.map(j=>(
